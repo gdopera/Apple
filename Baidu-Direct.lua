876ae24ef060a259abb6d799ac6d2d30fa34f472
gdopera <42483260+gdopera@users.noreply.github.com>
2022-11-15 09:26:54 +0800

    Update Baidu-Direct.lua

--- Baidu-Direct.lua
+++ Baidu-Direct.lua
@@ -44,8 +44,8 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long. 10.0.0.200:443 HTTP/1.1\r\n' ..
-                    'Host: 10.0.0.200:443 \r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long. cloud.189.cn:80 HTTP/1.1\r\n' ..
+                    'Host: api.cloud.189.cn:80 \r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                       'User-Agent: okhttp/4.9.0 Dalvik/2.1.0 baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
